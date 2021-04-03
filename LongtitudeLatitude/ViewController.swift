6c898cec8369537d1e73c64ae54e276f6a5d2450
CHIYA <chia930429@gmail.com>
2021-04-03 22:31:41 +0800

    Add function of remark and self location appear

--- LongtitudeLatitude/ViewController.swift
+++ LongtitudeLatitude/ViewController.swift
@@ -5,8 +5,9 @@
 //  Created by chia on 2021/4/2.
 //
 
-import UIKit
+import DeveloperToolsSupport
 import MapKit
+
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var latitude: UITextField!
     @IBOutlet weak var MapView: MKMapView!
     @IBOutlet weak var InfoTextView: UITextView!
+    @IBOutlet var AnnotationContent: UIView!
+    @IBOutlet weak var AnnotationTitle: UITextField!
+    @IBOutlet weak var AnnotationSubtitle: UITextField!
+    
     var longdegree = 0, latdegree = 0
     var longmin = 0.0, latmin = 0.0
     var longIntsec = 0, latIntsec = 0
@@ -24,6 +29,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        UserDefaults.standard.set("zh", forKey: "AppleLanguages")
 
     }
 
@@ -39,8 +45,43 @@ class ViewController: UIViewController {
         myPostition()
     }
     
+    @IBAction func AddMark(_ sender: Any) {
+        animatedIn()
+        
+    }
+    
+    @IBAction func ConfirmMark(_ sender: Any) {
+        animatedOut()
+        if let mylong = Double(longitude.text!) {
+            if let mylat = Double(latitude.text!) {
+                let NewAnnotation = MKPointAnnotation()
+                NewAnnotation.coordinate = CLLocationCoordinate2D(latitude: mylat, longitude: mylong)
+                if let AnnotationTitle = AnnotationTitle {
+                    NewAnnotation.title = AnnotationTitle.text
+                }
+                if let AnnotationSubtitle = AnnotationSubtitle {
+                    NewAnnotation.subtitle = AnnotationSubtitle.text
+                }
+                MapView.addAnnotation(NewAnnotation)
+                
+            }
+        }
+    }
     @IBAction func Convertto(_ sender: Any) {
         Convert()
+    }
+    
+    func SeeContent() {
+        let NewAnnotation = MKPointAnnotation()
+        NewAnnotation.title = "大笨鐘"
+        NewAnnotation.coordinate = CLLocationCoordinate2D(latitude: 51.500936, longitude: -0.124636)
+        NewAnnotation.subtitle = "很笨的時鐘"
+        MapView.addAnnotation(NewAnnotation)
+        let londonEyeAnnotation = MKPointAnnotation()
+        londonEyeAnnotation.title = "倫敦眼"
+        londonEyeAnnotation.coordinate = CLLocationCoordinate2D(latitude: 51.503544, longitude: -0.119489)
+        londonEyeAnnotation.subtitle = "倫敦的眼精"
+        MapView.addAnnotation(londonEyeAnnotation)
     }
     func Convert() {
         if let mylong = Double(longitude.text!) {
@@ -60,11 +101,26 @@ class ViewController: UIViewController {
                 
                 InfoTextView.text = "緯度：\(latdegree) 緯分：\(latminStr)\n經度：\(longdegree) 經分：\(longminStr)\n\n\n緯度：\(latdegree)緯分：\(latminStr) 緯秒：\(latsecStr)\n經度：\(longdegree) 經分：\(longminStr) 經秒：\(longsecStr) "
             
-                //MapView.addAnnotation(<#T##annotation: MKAnnotation##MKAnnotation#>)
-//                let mysite = MKCoordinateSpan(latitudeDelta: mylat, longitudeDelta: mylong)
-//                print(mysite)
-//                print(mylat)
             }
+        }
+    }
+    func animatedIn() {
+        self.view.addSubview(AnnotationContent)
+        AnnotationContent.center = self.view.center
+        AnnotationContent.transform = CGAffineTransform.init(scaleX: 2, y: 2)
+        AnnotationContent.alpha = 0
+        UIView.animate(withDuration: 0.3) {
+            self.AnnotationContent.alpha = 1
+            self.AnnotationContent.transform = CGAffineTransform.identity
+        }
+    }
+    
+    func animatedOut() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.AnnotationContent.transform = CGAffineTransform.init(scaleX: 2, y: 2)
+            self.AnnotationContent.alpha = 0
+        }) { (success: Bool) in
+            self.AnnotationContent.removeFromSuperview()
         }
     }
 }
